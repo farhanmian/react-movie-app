a33ec687fc4cf2ae97b5c7e6d522cdf7a4c8e6d3
farhanmian <farhanmian099@gmail.com>
2022-02-27 18:12:36 +0530

    fixed a bug

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 function Movie(props) {
-
-    return (
-        <Link id={props.id} to={`/${props.type === 'Tv Series' ? 'tv' : props.type.toLowerCase()}/${props.id}`} className={classes.Movie}>
-            <h4 className={classes.MovieRating}>{props.rating}</h4>
-            <img src={props.image} alt="MovieImage" />
-            <h3>{props.name}</h3>
-            <div>
-                <p>{props.type}</p> <p>{props.date}</p>
-            </div>
-        </Link>
-    );
+  return (
+    <Link
+      id={props.id}
+      to={`/${props.type === "Tv Series" ? "tv" : props.type.toLowerCase()}/${
+        props.id
+      }`}
+      className={classes.Movie}
+    >
+      <h4 className={classes.MovieRating}>{props.rating}</h4>
+      <img src={props.image} alt="MovieImage" />
+      <h3>
+        {props.name.trim().length > 23
+          ? `${props.name.slice(0, 23)}...`
+          : props.name}
+      </h3>
+      <div>
+        <p>{props.type}</p> <p>{props.date}</p>
+      </div>
+    </Link>
+  );
 }
 
 export default Movie;
