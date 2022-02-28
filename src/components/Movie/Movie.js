579bf20c36ee6c9c547ccd9233abcd1e9dc8f5f0
farhanmian <farhanmian099@gmail.com>
2022-02-28 21:08:48 +0530

    change some styling

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./Movie.module.css";
 
 function Movie(props) {
+  const [nameLimit, setNameLimit] = useState(23);
+
+  useEffect(() => {
+    if (window.innerWidth <= 1000) {
+      setNameLimit(15);
+    }
+  }, []);
+
   return (
     <Link
       id={props.id}
@@ -15,11 +23,11 @@ function Movie(props) {
       <h4 className={classes.MovieRating}>{props.rating}</h4>
       <img src={props.image} alt="MovieImage" />
       <h3>
-        {props.name.trim().length > 23
-          ? `${props.name.slice(0, 23)}...`
+        {props.name.trim().length > nameLimit
+          ? `${props.name.slice(0, nameLimit)}...`
           : props.name}
       </h3>
-      <div>
+      <div className={classes.MovieTypeContainer}>
         <p>{props.type}</p> <p>{props.date}</p>
       </div>
     </Link>
